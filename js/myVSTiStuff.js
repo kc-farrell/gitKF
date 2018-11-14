cf34edc162f7f08ecaaee7021c12ccbc0485c559
Kevin Farrell <kevin.farrell@qsrsoft.com>
2018-11-14 14:39:41 -0600

    This version writes VSTi object key-value pairs to console to demonstrate dynamic retrieval of VSTi data when the objects ID may not be known

--- js/myVSTiStuff.js
+++ js/myVSTiStuff.js
@@ -7,40 +7,36 @@ $( document ).ready(function() {
 	/* Fetch all localStorage VSTi records. */
 	function loadPlugins() {
 		//JSON.stringify(localStorage); not needed, already done @ #151 below
-	
-	/*	console.log('hey ' + localStorage.key(5)); 
-		 ABOVE IS STATIC: (i) gets the first one or 5 will get the fifth one because its not looping.
 
-		for (var i=0, vCounter=localStorage.length; i<vCounter; ++i) { 
-		    var storageKey = localStorage.key(i);
-		    var 
-		    console.log( storageKey + ': ' + localStorage.getItem(storageKey) );
-		}	
-*/
+		/* Set VARS */
+		var o = localStorage;
+		var i = 0;
+		var keyIndex = Object.keys(o)[i];
+		var dParsed = JSON.parse(o[keyIndex]).description;
+		var stuff = 'stuff';
 
-var counter = 0;
-o = localStorage;
-console.log('Here\'s the list: ')
-//console.log(Object.keys(o).toString()); // key1 value1
+		console.table(o);
 
-for(var blowme in o) {
-  if(o.hasOwnProperty(blowme)) {
-    console.log(blowme); // Dont ask me why blowme still works. whats in a name?
-  }
-}
+		console.log('vsti Name: ' + keyIndex + '\n' + 'Description: ' + dParsed); // Testing - just gets first item, outside the forin loop.			
 
-		
-
-		// iterate localStorage
-		/*
-		for (var i = 0; i < localStorage.length; i++) {
-		  // set iteration key name
-		  var description = localStorage.key(i);
-		  console.log(i + " : " + description + " = " + localStorage.getItem(description)	);  
+		for (var key in o) {
+		  if (o.hasOwnProperty(key)) {
+		  	var sand = JSON.parse(o[key])["name"] + JSON.parse(o[key])["description"];
+			console.log('<div id=" + JSON.parse(o[key])['obj'] + ">' +
+				'\n' + 'vsti Name: ' + JSON.parse(o[key])["name"] + 
+				'\n' + 'Description: ' + JSON.parse(o[key])["description"] + 
+				'\n' + 'Version: ' + JSON.parse(o[key])["version"] +
+				'\n' + 'Rating: ' + JSON.parse(o[key])["rating"] + 
+				'\n' + 'Review: ' + JSON.parse(o[key])["review"] );							
+			document.getElementById('sandbox').append(sand);
+			// Displays Properties, values for consumption by another function
+		  }
+		  else {
+		   console.log('ELSE ' + key); // these are from __prototype__
+		 }
 		}
-		*/	
-
 	}
+
 	/* Setup up Event Listeners here */
 	/* Triggered when user clicks ADD NEW VSTI button from the main page. */
 	document.getElementById('addNewVstiBtn').addEventListener('click', popVSTiModal);			
@@ -85,24 +81,25 @@ for(var blowme in o) {
 
 	function submitNewVSTi() {
 		var vForm = document.getElementById('vstiEntry');
-		var dObjID = 'vsti-' + Math.floor(Math.random() * 900);	
+		var dObjID = 'vsti-' + Math.floor(Math.random() * 900);	//create a var to become the object's ID/key
 
 		console.log('The randomly generated ID that will get applied to the cloned dataRow \(aka dObjID\) is : ' + dObjID);	//prove the random generator is working	
 
 		var editBtn = 'editBtn' + dObjID.substr(4);
 
 		/* Provide common names for use in dataRow innerHTML (values) manipulation */	
-		var dVSTiName = document.getElementById('vstiName').value;
-		var dVSTiVersion = document.getElementById('vstiVersion').value;
-		var dVSTiDescription = document.getElementById('vstiDescription').value;
-		var dVSTiFormat = document.getElementById('vstiFormat').value;
-		var dVSTiRating = document.getElementById('vstiRating').value;
-		var dVSTiReview	= document.getElementById('vstiReview').value;
+		var dVSTiName 			= document.getElementById('vstiName').value; // modal value
+		var dVSTiVersion 		= document.getElementById('vstiVersion').value; // modal value
+		var dVSTiDescription 	= document.getElementById('vstiDescription').value; // modal value
+		var dVSTiFormat 		= document.getElementById('vstiFormat').value; // modal value
+		var dVSTiRating 		= document.getElementById('vstiRating').value; // modal value
+		var dVSTiReview			= document.getElementById('vstiReview').value; // modal value
+		var dVSTiHTML; // dynamically-generated HTML to follow, from below.		
 
 		console.log('    Entering addNewVSTi() = Create a new cloned dataRow and apply the dynamic ID to the row.');
 
 		/* Create the prototype constructor object (NOT an Object itself) using dynamic values to build new objects from. */									
-		function Vsti(obj, btn, name, version, description, format, rating, review, vmethod) {
+		function VstiObject(obj, btn, name, version, description, format, rating, review, htmlCode, vmethod) {
 			this.obj = dObjID;
 			this.btn = editBtn; 
 			this.name = name;
@@ -111,6 +108,7 @@ for(var blowme in o) {
 			this.format = format;
 			this.rating = rating;
 			this.review = review;
+			this.htmlCode = "";
 			this.vmethod = function() {
 				console.log( 'HELLO new VSTi Object, why not introduce yourself? Go ahead and say a few words...' + '\n');
 				console.log('     Uhhh-huhhh....um, yeah... OK, I don\'t' + ' really like talking about myself. Hi. My name is ' 
@@ -121,68 +119,65 @@ for(var blowme in o) {
 		}	
 
 		/********** Build the new object from the prototype */
-		var dynamicVSTiObject = new Vsti(dObjID, editBtn, dVSTiName, dVSTiVersion, dVSTiDescription, dVSTiFormat, dVSTiRating, dVSTiReview);				
+		var dynamicVSTiObject = new VstiObject(dObjID, editBtn, dVSTiName, dVSTiVersion, dVSTiDescription, dVSTiFormat, dVSTiRating, dVSTiReview, dVSTiHTML);				
 		dynamicVSTiObject.vmethod();  // Run the new VSTIs function (method)
 
-		console.log('    Clone the dataRow (template) and generate a new dataRow with a dynamic ID.');
-
-		/********** Clone the dataRow (template) w default values */
-		var sourceRow = document.getElementsByClassName('dataRow')[0]; // Grab the first dataRow in the document (hidden div) to use as HTML template 
-		var destination = document.getElementById('outputList'); // Set destination for the cloned div 
-		var cln = sourceRow.cloneNode(true); // Duplicate the sourceRows contents
-		destination.appendChild(cln); // and append the outputList div with the cloned template row 					
-					
-		var myNewRow = destination.lastChild; // After cloning row, get the latest dataRow from the page 
-		var myNewID = dObjID; // Grab the dynamic ID
-		myNewRow.setAttribute("id", myNewID); // Apply the dynamic ID to the dataRow
-
-		/********** Set form field values based on entries */
-
-		/********** Create common names for referring to el IDs */
-		var newRowID 		= document.getElementById(myNewID);
-		var myNewBtn 		= newRowID.querySelector('.btn').setAttribute("id", editBtn);			
-		var newName 		= newRowID.querySelector('.dVSTiName');
-		var newVersion 		= newRowID.querySelector('.dVSTiVersion');
-		var newDescription	= newRowID.querySelector('.dVSTiDescription');
-		var newFormat 		= newRowID.querySelector('.dVSTiFormat');
-		var newRating 		= newRowID.querySelector('.dVSTiRating');
-		var newReview 		= newRowID.querySelector('.dVSTiReview');
-
-		/* Set event listener on the new button */
-		var clickThisBtn = document.getElementById(editBtn);
-		clickThisBtn.addEventListener("click", editThisVsti);
 
 		function buildTheRow() {
 			console.log('    ENTERING buildTheRow func ...');
 
-			/*Set localStorage from the new prototype object */
-			localStorage.setItem(dObjID, JSON.stringify(dynamicVSTiObject)); // 1) Set the storage objects name using prev generated dynamic ID 2) STRINGIFY IT so you can use it.
+			console.log('    Clone the dataRow (template) and generate a new dataRow with a dynamic ID.');
 
-			/* Confused. Thought this was needed. Followup.
-				var vstiList = JSON.parse(localStorage.getItem(dynamicVSTiObject)); // Get the localStorage object so we can mess with it.
-			*/
+			/********** Clone the dataRow (template) w default values */
+			var sourceRow 	= document.getElementsByClassName('dataRow')[0]; // Grab the first dataRow in the document (hidden div) to use as HTML template 
+			var destination = document.getElementById('outputList'); // Set destination for the cloned div 
+			var cln 		= sourceRow.cloneNode(true); // Duplicate the sourceRows contents
+			destination.appendChild(cln); // and append the outputList div with the cloned template row 					
+						
+			var myNewRow 	= destination.lastChild; // After cloning row, get the latest dataRow from the page 
+			var myNewObjID 	= dObjID; // Grab the dynamic ID
+			myNewRow.setAttribute("id", myNewObjID); // Apply the dynamic ID to the cloned dataRow (e.g. "destination").
 
-			var pluginName = dynamicVSTiObject["name"];
-			var pluginVersion = dynamicVSTiObject["version"];
-			var pluginFormat = dynamicVSTiObject["format"];		
-			var pluginDescription = dynamicVSTiObject["description"];
-			var pluginReview = dynamicVSTiObject["review"];
+			/********** Set form field values based on entries */
 
-			console.error('******************** Prove that it got inserted into localStorage. The item added includes: ' + 
+			/********** Create common names for referring to el Ids and Classes */
+			var newRowID 		= document.getElementById(myNewObjID); // use newRowID to setup references to later drop values into.
+			var newRowHTML		= newRowID.innerHTML;
+			var myNewBtn 		= newRowID.querySelector('.btn').setAttribute("id", editBtn); // use newRowId to target classname based on the row's ID.	
+			var newName 		= newRowID.querySelector('.dVSTiName'); // use newRowId to target classname based on the row's ID.
+			var newVersion 		= newRowID.querySelector('.dVSTiVersion'); // use newRowId to target classname based on the row's ID.
+			var newDescription	= newRowID.querySelector('.dVSTiDescription'); // use newRowId to target classname based on the row's ID.
+			var newFormat 		= newRowID.querySelector('.dVSTiFormat'); // use newRowId to target classname based on the row's ID.
+			var newRating 		= newRowID.querySelector('.dVSTiRating'); // use newRowId to target classname based on the row's ID.
+			var newReview 		= newRowID.querySelector('.dVSTiReview'); // use newRowId to target classname based on the row's ID.
+
+			/* Set event listener on the new button */
+			var clickThisBtn = document.getElementById(editBtn);
+			clickThisBtn.addEventListener("click", editThisVsti);
+
+			var pluginHTML			= newRowID.innerHTML;
+/*
+			console.log('******************** Prove localStorage insert worked. The item added to this lSo includes these values: ' + 
 				'\n' + pluginName +
 				'\n' + pluginVersion +
 				'\n' + pluginFormat +
-				'\n' + pluginDescription +
+				'\n' + pluginDescription + 
+				'\n' + pluginRating +				
 				'\n' + pluginReview
 			);	
+*/
+			/* Output new data to the outputlist div */
+			newName.innerHTML 			= dynamicVSTiObject.name;
+			newVersion.innerHTML 		= dynamicVSTiObject.version;
+			newDescription.innerHTML 	= dynamicVSTiObject.description;
+			newFormat.innerHTML 		= dynamicVSTiObject.format;
+			newRating.innerHTML 		= dynamicVSTiObject.rating;
+			newReview.innerHTML 		= dynamicVSTiObject.review;
 
-			newName.innerHTML = dynamicVSTiObject.name;
-			newVersion.innerHTML = dynamicVSTiObject.version;
-			newDescription.innerHTML = dynamicVSTiObject.description;
-			newFormat.innerHTML = dynamicVSTiObject.format;
-			newRating = dynamicVSTiObject.rating;
-			newReview = dynamicVSTiObject.review;
+		localStorage.setItem(dObjID, JSON.stringify(dynamicVSTiObject)); 
+		/* Set localStorage from the new prototype object created above (dynamicVSTiObject); stringify it, so you can use it to retrieve key/property values. */			
 
+			console.log('the HTML survey says : ' + pluginHTML);
 		}	
 		buildTheRow();
 
@@ -217,96 +212,5 @@ for(var blowme in o) {
 	
 	}
 
-
-
 });// End: 	DocReady
-
-
-
-/*
- LOCAL STORAGE NOTES: =======================================================================
-
-var test = { test: "thing", test2: "thing2", test3: [0, 2, 44] }​​​​​​​; // Create the storage array object
-localStorage.setItem("test", JSON.stringify(test)); //Set it and stringify it
-var test2 = localStorage.getItem("test"); // fetch the property/ies you need
-console.log(test2); // Test your work - logs "{"test":"thing","test2":"thing2","test3":[0,2,44]}"
-
-
-this works:
-var theList = Storage;
-console.log(theList.hasOwnProperty("name")); //returns true 
-
-var theList = Storage;
-console.log("name" in theList); // test for existence of name property (boolean test)
-console.log(theList.name); // reveal the stored object's name.
-console.log(Object.values(theList));
-
-var temp = JSON.parse(localStorage.getItem('vsti-106'));
-console.log(temp.name);
-console.log(temp.obj);
-console.log(Object.keys(temp));
-
-var temp = JSON.parse(localStorage.getItem(	localStorage.key(3)	));
-console.log(temp.name);
-console.log(temp.obj);
-console.log(Object.keys(temp));
-
-
-
-
-Helpful Resources that Explain This Stuff: =======================================================================
--http://adripofjavascript.com/blog/drips/the-uses-of-in-vs-hasownproperty.html
--https://stackoverflow.com/questions/40603019/how-do-we-find-the-various-properties-of-localstorage-in-html5
-
-
-
-
-	// show key/value pairs as an object:
-	for(var i =0; i < localStorage.length; i++){
-	   console.log(	localStorage.getItem(localStorage.key(i)	)	);
-	}
-
-	// iterate localStorage
-	
-	for (var i = 0; i < localStorage.length; i++) {
-	  // set iteration key name
-	  var key = localStorage.newObjID(i);
-	  // use key name to retrieve the corresponding value
-	  var value = localStorage.getItem(key);
-	  // console.log the iteration key and value
-	  console.log('Key: ' + key + ', Value: ' + value);  
-	}
-
-	//Use the values from localStorage 
-	document.getElementById("test").innerHTML = localStorage.getItem("name1");
-	console.log('the damn thing is ' + newObjID.description);
-
-	//Iterate through properties:
-	//============================
-	for(var i=0; i < localStorage.length; i++){
-	var propertyName = localStorage.key(i);
-	console.log(  i + " : " + propertyName + " = " +
-	localStorage.getItem(propertyName));
-	}
-
-	//Storage Event Listener: (ONLY WORKS WHEN MORE THAN ONE WINDOW/TAB IS OPEN FOR THE SAME PG, like it tells the OTHER WINDOWS an update occurred. Weird.)
-	// https://stackoverflow.com/questions/40603019/how-do-we-find-the-various-properties-of-localstorage-in-html5
-	function onStorageEvent(storageEvent){
-	    alert("storage event");
-	}
-
-	window.addEventListener('storage', onStorageEvent, false); 
-
-	//Using localStorage as a DB so VSTIs are avail when page reloads
-	//	Setting LS Values::
-	//	Using an -Existing- object and storing via localStorage:
-	//	window.localStorage.setItem('user', JSON.stringify(person));
-	//
-	//	Creating a -New- object and storing via localStorage:
-	//	localStorage.setItem('thisThing', JSON.stringify({'myKey':'myValue'}))
-	//	
-	//	Getting LS Values::
-	//	var retrievedObject = localStorage.getItem(obj);
-	//	console.log(obj, JSON.parse(obj));
-*/	
 
